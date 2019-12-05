0492d16fa0be4fb119c4a691c9a52343e354b652
SarahF <sarahfrerichs@gmail.com>
2019-12-04 23:57:42 -0600

    finished JobField base class

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,54 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+abstract public class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    private JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // Custom toString, equals, and hashCode methods:
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Location)) return false;
+        Location location = (Location) o;
+        return getId() == location.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    //getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -1,56 +1,9 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
+public class Location extends JobField {
 
-public class Location {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Location() {
-        id = nextId;
-        nextId++;
+    public Location(String value) {
+        super(value);
     }
-
-    public Location(String aValue) {
-        this();
-        this.value = aValue;
-    }
-
-
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Location)) return false;
-        Location location = (Location) o;
-        return getId() == location.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 
 }
