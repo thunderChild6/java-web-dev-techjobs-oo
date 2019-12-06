6d07f263fd1ffcee0d6e373eecef63eaeb4068c1
SarahF <sarahfrerichs@gmail.com>
2019-12-06 00:19:53 -0600

    Changed back to initial

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,7 +1,5 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
-
 public class Job {
 
     private int id;
@@ -13,87 +11,13 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // constructors:
+    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
+    //  other five fields. The second constructor should also call the first in order to initialize
+    //  the 'id' field.
 
-    public Job() {
-        id = nextId;
-        nextId++;
-    }
+    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
+    //  match.
 
-    public Job(String aName, Employer aEmployer, Location aLocation, PositionType aPositionType, CoreCompetency aCoreCompetency) {
-        this();
-        this.id = id;
-        this.name = aName;
-        this.employer = aEmployer;
-        this.location = aLocation;
-        this.positionType = aPositionType;
-        this.coreCompetency = aCoreCompetency;
-    }
-
-    // equals and hashcode methods:
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Job job = (Job) o;
-        return id == job.id &&
-                Objects.equals(name, job.name) &&
-                Objects.equals(employer, job.employer) &&
-                Objects.equals(location, job.location) &&
-                Objects.equals(positionType, job.positionType) &&
-                Objects.equals(coreCompetency, job.coreCompetency);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, employer, location, positionType, coreCompetency);
-    }
-
-
-    // Getters and setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Employer getEmployer() {
-        return employer;
-    }
-
-    public void setEmployer(Employer employer) {
-        this.employer = employer;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public PositionType getPositionType() {
-        return positionType;
-    }
-
-    public void setPositionType(PositionType positionType) {
-        this.positionType = positionType;
-    }
-
-    public CoreCompetency getCoreCompetency() {
-        return coreCompetency;
-    }
-
-    public void setCoreCompetency(CoreCompetency coreCompetency) {
-        this.coreCompetency = coreCompetency;
-    }
+    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+    //  and id.
 }
--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,56 +1,7 @@
 package org.launchcode.techjobs_oo.Tests;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.launchcode.techjobs_oo.*;
-
-import static junit.framework.TestCase.*;
-
 public class JobTest {
 
-    Job empty_test_job_one;
-    Job empty_test_job_two;
-    Job new_job_object;
-    Job test_equality_job_object_one;
-    Job test_equality_job_object_two;
 
-    @Before
-    public void createJobObject() {
-        empty_test_job_one = new Job();
-        empty_test_job_two = new Job();
-        new_job_object = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        test_equality_job_object_one = new Job("A", new Employer("B"), new Location("C"), new PositionType("D"), new CoreCompetency("E"));
-        test_equality_job_object_two = new Job("A", new Employer("B"), new Location("C"), new PositionType("D"), new CoreCompetency("E"));
-    }
-
-    @Test
-    public void testSettingJobId() {
-        assertEquals(1, empty_test_job_one.getId());
-        assertEquals(2, empty_test_job_two.getId());
-    }
-
-    @Test
-    public void testJobConstructorSetsAllFields() {
-        assertEquals(3, new_job_object.getId());
-
-        assertEquals("Product tester", new_job_object.getName());
-
-        assertTrue(new_job_object.getEmployer() instanceof Employer);
-        assertEquals("ACME", new_job_object.getEmployer().getValue());
-
-        assertTrue(new_job_object.getLocation() instanceof Location);
-        assertEquals("Desert", new_job_object.getLocation().getValue());
-
-        assertTrue(new_job_object.getPositionType() instanceof PositionType);
-        assertEquals("Quality control", new_job_object.getPositionType().getValue());
-
-        assertTrue(new_job_object.getCoreCompetency() instanceof CoreCompetency);
-        assertEquals("Persistence", new_job_object.getCoreCompetency().getValue());
-    }
-
-    @Test
-    public void testJobsForEquality() {
-        assertFalse(test_equality_job_object_one.equals(test_equality_job_object_two));
-    }
 
 }
